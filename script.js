acd65e2e39b3b0f1a41b1930f5947c2c63c74df0
anant-jain2011 <kumkumnidhi14@gmail.com>
2022-11-30 22:01:12 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-let pokemon = prompt("Enter Pokemon name:- ");
+let pokemon = "lapras"; //prompt("Enter Pokemon name:- ");
 
 const prints = (param) => {
     console.log(param);
 }
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } 
+
+      else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
 
 fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
     .then(res => res.json())
@@ -11,8 +36,10 @@ fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
         image.src = data.sprites.versions["generation-ii"].crystal.front_transparent;
     });
 
-fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+fetch("https://pokeapi.co/api/v2/pokemon")
     .then(res => res.json())
-    .then(data => prints(data.sprites.versions["generation-ii"].crystal));
+    .then(data => {
+        prints(data)
+    });
 
 // document.body.appendChild(image);
