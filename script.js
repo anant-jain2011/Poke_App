1072d0c5378d8b143b976743324522ce3a87e76a
anant-jain2011 <kumkumnidhi14@gmail.com>
2022-11-30 20:22:13 +0530

    a1

---
+++ script.js
@@ -0,0 +1,18 @@
+// let pokemon = prompt("Enter Pokemon name:- ");
+
+// const prints = (param) => {
+//     console.log(param);
+// }
+
+// fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+//     .then(res => res.json())
+//     .then((data) => {
+//         pname.innerHTML = data.name[0].toUpperCase() + data.name.slice(1);
+//         image.src = data.sprites.versions["generation-ii"].crystal.front_transparent;
+//     });
+
+// fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+//     .then(res => res.json())
+//     .then(data => prints(data.sprites.versions["generation-ii"].crystal));
+
+// // document.body.appendChild(image);
